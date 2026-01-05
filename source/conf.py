89fcda0ab94714cf9b37d1feda681f2afbfd2fcc
educazioneaperta <redazione.educazioneaperta@gmail.com>
2026-01-05 18:28:10 +0100

    Titolo

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Lumina rebus'
+project = 'La natura delle cose'
 copyright = '2026, Antonio Vigilante'
 author = 'Antonio Vigilante'
 
